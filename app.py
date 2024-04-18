9d725d51a223cb3fd307e9d6d11006e416f69b9f
Roberta Sgariglia <robertasgariglia@Robertas-MacBook-Pro.local>
2024-04-18 17:23:45 +0200

    adding code to disable made with streamlit banner

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ st.set_page_config(layout="wide", page_title="Emissions App", initial_sidebar_st
 credentials = st.secrets["gcp_service_account"]
 project_id = credentials["project_id"]
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 #specifying sidebar layout
 show_pages(
